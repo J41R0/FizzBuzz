7ac56676a021b4298fe5ddfb65b670150b271934
J41R0 <jairo.lefebre@gmail.com>
2020-01-26 17:46:08 -0500

    Adding problem methods

---
+++ fizz_buzz.py
@@ -0,0 +1,24 @@
+def is_divisible(number, divisor):
+    """
+    Validate if number is divisible by divisor
+    Args:
+        number: int number
+        divisor: int number
+
+    Returns: True if number is divisible by divisor; False otherwise
+
+    """
+    pass
+
+
+def fizz_buzz_game(number):
+    """
+    If number is divisible by 3 return 'Fizz', if divisible by 5 return 'Buzz' and if divisible by 3 and 5 return
+     'FizzBuzz'. In any other situation return number
+    Args:
+        number: int number
+
+    Returns: A word if number is divisible by 3, 5 or both; number otherwise
+
+    """
+    pass
