6991d43c17264391ada34af27e79111d24dda003
J41R0 <jairo.lefebre@gmail.com>
2020-01-26 17:11:21 -0500

    Add tests methods

---
+++ test_fizz_buzz.py
@@ -0,0 +1,23 @@
+import unittest
+from fizz_buzz import fizz_buzz_game
+
+
+class FizzBuzzTest(unittest.TestCase):
+    def test_input(self):
+        pass
+
+    def test_numbers(self):
+        pass
+
+    def test_fizz(self):
+        pass
+
+    def test_buzz(self):
+        pass
+
+    def test_fizz_buzz(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
